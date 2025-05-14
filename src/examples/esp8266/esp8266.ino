c8626442b686eaefd12026b8662a029966c7645a
Ezequiel Abregu <ezequielabregu.code@gmail.com>
2025-05-13 21:37:17 -0300

    add esp8266 example test

---
+++ src/examples/esp8266/esp8266.ino
@@ -0,0 +1,15 @@
+#include <MIDI.h>
+
+MIDI_CREATE_DEFAULT_INSTANCE();
+
+void setup() {
+  MIDI.begin(MIDI_CHANNEL_OMNI);
+  Serial.begin(115200); // USB serial
+}
+
+void loop() {
+  MIDI.sendNoteOn(60, 100, 1);
+  delay(250);
+  MIDI.sendNoteOff(60, 0, 1);
+  delay(250);
+}
